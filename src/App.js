0393af3eed1bd6e7b1ecc8ef02ec63304c4ae98a
jhell85 <jhell85@gmail.com>
2020-08-07 21:32:06 -0700

    add accordion component and passed items props down to it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 
-const items = [{}];
+const items = [
+  {
+    title: "what is React?",
+    content: "React is a front end JavaScript framework",
+  },
+];
 
 export default () => {
-  return <h1>app</h1>;
+  return (
+    <h1>
+      <Accordion items={items} />
+    </h1>
+  );
 };
