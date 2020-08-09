b3316cdf170984aba8af56be009fe08586364e9c
jhell85 <jhell85@gmail.com>
2020-08-08 21:01:18 -0700

    add stylesheet link and classes to elements.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ const items = [
     title: "what is React?",
     content: "React is a front end JavaScript framework",
   },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
 ];
 
 export default () => {
   return (
-    <h1>
+    <div>
       <Accordion items={items} />
-    </h1>
+    </div>
   );
 };
