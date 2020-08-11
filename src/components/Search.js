8a87f38916866a7390f4e55ed85614f9a1505d8e
jhell85 <jhell85@gmail.com>
2020-08-11 12:35:02 -0700

    add additional useEffect to watch when term is changed and a debounced state when a user types into the input

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,18 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -13,23 +24,29 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-    if (term && !results.length) {
+    if (term) {
       search();
     }
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        search();
-      }
-    }, 500);
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [term]);
+  }, [debouncedTerm]);
+
+  // useEffect(() => {
+  //   if (term && !results.length) {
+  //     search();
+  //   }
+  //   const timeoutId = setTimeout(() => {
+  //     if (term) {
+  //       search();
+  //     }
+  //   }, 500);
+  //   return () => {
+  //     clearTimeout(timeoutId);
+  //   };
+  // }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
