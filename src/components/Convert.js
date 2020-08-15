1c8039d38583ac495a0bd1a3b875f61792381915
jhell85 <jhell85@gmail.com>
2020-08-15 11:24:52 -0700

    add api call  for translation of text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
   useEffect(() => {
-    console.log("new language or text");
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
   }, [language, text]);
-  return <div />;
+  return (
+    <div>
+      <h2 className="ui header">{translated}</h2>
+    </div>
+  );
 };
 
 export default Convert;
