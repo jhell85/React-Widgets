79e999f8ed835edd2c69cc6729390b1a52d398a1
jhell85 <jhell85@gmail.com>
2020-08-15 17:38:12 -0700

    add link and for navbar header and route component to navigate to appropriate URL

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 const Route = ({ path, children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.path);
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", onLocationChange);
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
+
   return window.location.pathname === path ? children : null;
 };
 export default Route;
