a6652c1e0e8535f9628424b181ee3ff7be5c6b8b
jhell85 <jhell85@gmail.com>
2020-11-15 18:36:53 -0800

    Updates

---
+++ precache-manifest.4382998c4256b11c0d99f4cb94f77d89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0af7aee5b6ac2f7f694a417d32d2aea",
+    "url": "/React-Widgets/index.html"
+  },
+  {
+    "revision": "bf0a8ea65c7add0c225d",
+    "url": "/React-Widgets/static/js/2.722102a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Widgets/static/js/2.722102a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c89a51e7e69d44e6273",
+    "url": "/React-Widgets/static/js/main.f745bd7e.chunk.js"
+  },
+  {
+    "revision": "6ab4cf65074b026b9af3",
+    "url": "/React-Widgets/static/js/runtime-main.125720c2.js"
+  }
+]);
